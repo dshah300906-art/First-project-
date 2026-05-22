ea10ed80eb46d736f673e0b5aa2122e3c7cec6b3
Dhruvi shah <dshah300906@gmail.com>
2026-05-22 21:45:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Productlist from 'Productlist'
+
+
+    
+    function App() {
+  return (
+    <>
+    <div>
+      <h1>Product List</h1>
+
+      <ProductList />
+    </div>
+    
+  
+  
+
+     
+
+
+    </>
+  )};
+  
+  
+
+
+export default App
---
+++ Productlist.jsx
@@ -0,0 +1,60 @@
+function Productlist()
+{
+     const products = [
+    {
+      id: 1,
+      title: "iPhone 15",
+      price: 79999,
+      rating: 4.8,
+      image:
+        "hero.png",
+    },
+    {
+      id: 2,
+      title: "Laptop",
+      price: 55999,
+      rating: 4.5,
+      image:
+        "hero.png",
+    },
+    {
+      id: 3,
+      title: "Headphones",
+      price: 2999,
+      rating: 4.2,
+      image:
+        "hero.png",
+    },
+  ];
+}
+
+function ProductCard(p) {
+  return (
+    <div>
+      <img src={p.image} alt={p.title}  />
+
+      <h2>{p.title}</h2>
+
+      <p>Price: ₹{p.price}</p>
+
+      <p>Rating: ⭐ {p.rating}</p>
+    </div>
+  );
+}
+
+  return (
+    <div>
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          title={product.title}
+          price={product.price}
+          rating={product.rating}
+          image={product.image}
+        />
+      ))}
+    </div>
+  );
+
+  export default ProductCard
+
