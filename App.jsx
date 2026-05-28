ac9c2134b1200dfe2b9ff182258bdda3105fd210
Dhruvi shah <dshah300906@gmail.com>
2026-05-28 18:00:55 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -3,28 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Productlist from 'Productlist'
 
+function App() {
+  
 
-    
-    function App() {
   return (
     <>
+    function App() {
+  return (
     <div>
       <h1>Product List</h1>
 
       <ProductList />
     </div>
-    
-  
-  
+  )};
 
      
-
-
     </>
-  )};
-  
   
 
 
